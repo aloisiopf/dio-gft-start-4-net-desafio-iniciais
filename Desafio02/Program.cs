1077ab6b78d761191340724f059426b495c13348
aloisiopf <aloisiopf@gmail.com>
2022-03-13 18:15:52 -0300

    Desenvolvimento da Solução do Desafio 02 - Quadrado e ao Cubo

--- Desafio02/Program.cs
+++ Desafio02/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int inicio = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"{Math.Pow(i,1)} {Math.Pow(i,2)} {Math.Pow(i,3)}");
+            }
+        }
+    }
+}
